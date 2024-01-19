95f829424a68b5a6b9b5a4c418c2d676e5e84dde
praveen <praveenchouhan7272@gmail.com>
2024-01-19 21:50:20 +0530

    bulb project

---
+++ script.js
@@ -0,0 +1,19 @@
+var bulb = document.querySelector("#bulb");
+
+var btn =  document.querySelector("#btn");
+
+var flag = 0 ;
+
+btn.addEventListener("click" , function() {
+
+    if(flag==0){
+        bulb.style.backgroundColor = "yellow";
+        flag = 1;
+        btn.innerHTML = "OFF"
+
+    }else{
+        bulb.style.backgroundColor = "transparent";
+        flag = 0;
+        btn.innerHTML = "ON"
+    }
+})
